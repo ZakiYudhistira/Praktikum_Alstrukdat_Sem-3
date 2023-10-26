f402d3baf37b05711bb137c9934b08004ac72072
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2023-10-26 16:24:45 +0700

    test read word

---
+++ praktikum_5/bacaKata.c
@@ -0,0 +1,11 @@
+#include "charmachine.h"
+#include "wordmachine.h"
+#include "boolean.h"
+#include <stdio.h>
+
+int main() {
+    STARTWORD();
+    // if(currentWord ){
+    //     printf("Stringnya kosong mas bro\n")
+    // }
+}
